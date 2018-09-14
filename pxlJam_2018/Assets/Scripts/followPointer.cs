e4304bcd17c7af5229b90936600d24c31d08d35d
JhamesBrown <jamestron@gmail.com>
2018-09-14 21:30:55 +1200

    added folders and mvp scripts

---
+++ pxlJam_2018/Assets/Scripts/followPointer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followPointer : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
