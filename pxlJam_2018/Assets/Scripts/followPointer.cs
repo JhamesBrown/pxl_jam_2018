bb8a103e23f822f1c53952eeb8d7d7af8f4cb7ec
JhamesBrown <jamestron@gmail.com>
2018-09-15 16:26:23 +1200

    cat animations and directional logic

--- pxlJam_2018/Assets/Scripts/followPointer.cs
+++ pxlJam_2018/Assets/Scripts/followPointer.cs
@@ -7,6 +7,7 @@ public class followPointer : MonoBehaviour {
 
     public Transform target;
     public float speed = 1.0f;
+    public Transform visuals;
 
     private Rigidbody2D rb;
 
@@ -25,6 +26,9 @@ public class followPointer : MonoBehaviour {
     Vector2 getTargetDirection()
     {
         Vector2 v2 = target.position - gameObject.transform.position;
+        float direction = Mathf.Round(v2.normalized.x);
+        if (direction != 0)
+            visuals.localScale *= new Vector2(visuals.localScale.x * -direction, 1.0f);
         return v2.normalized;
     }
 }
