7155203df8584e5f686195feee516445aa0982d1
James <jamestron@gmail.com>
2018-09-16 16:43:47 +1200

    stages 1-3 made

--- pxlJam_2018/Assets/Scripts/stage_goal.cs
+++ pxlJam_2018/Assets/Scripts/stage_goal.cs
@@ -1,16 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class stage_goal : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+
+
+    private stageManager reachStageManager;
+    private Audio_System_Script reachAudioSystemScript;
+    
+    void Start () {
+        reachStageManager = stageManager.FindObjectOfType<stageManager>();
+        reachAudioSystemScript = Audio_System_Script.FindObjectOfType<Audio_System_Script>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+
+    public void CheckIfLast()
+    {
+        int sceneAmount = SceneManager.sceneCountInBuildSettings - 1;
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+
+
+
+        if (currentScene == sceneAmount)
+        {
+            reachAudioSystemScript.SFXcatSleep();
+            reachStageManager.ChangeMenus(stageManager.currentGameState.Win);
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            // int sceneindex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+
+    }
+
+
 }
