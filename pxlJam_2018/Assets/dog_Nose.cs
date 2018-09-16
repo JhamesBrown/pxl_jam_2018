9c040bc5f7f887020fc88dee69446ac9e2b78576
OlivierYC <olivier.y@chrun.com>
2018-09-16 15:22:27 +1200

    Dog Audio and Prefab update.

---
+++ pxlJam_2018/Assets/dog_Nose.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dog_Nose : MonoBehaviour {
+
+    private Audio_System_Script reachAudioSystemScript;
+
+    // Use this for initialization
+    void Start () {
+        reachAudioSystemScript = Audio_System_Script.FindObjectOfType<Audio_System_Script>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Cat")
+        {
+            reachAudioSystemScript.SFXdogAlert();
+        }
+    }
+}
